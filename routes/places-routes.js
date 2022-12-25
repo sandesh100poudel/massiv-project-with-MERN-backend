882571b0534b7d12f012c909582a5f9f772fa0bb
sandesh poudel <sandesh100poudel@gmail.com>
2022-12-25 09:09:32 +0545

    getting a place by userId

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,8 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
+const DUMMY_PLACES = [{
+    id:"p1",
+    title:"Empire state building",
+    description:"one of the most",
+    location:{
+        lat:40,
+        lng:-67
+    },
+    address:"laxamangung",
+    creator:"u1"
+}]
+
 router.get('/',(req,res,next)=>{
     res.json({message:"its working"});
 });
+
+router.get('/:pid',(req,res,next)=>{
+    const placeId = req.params.pid;
+    const place=DUMMY_PLACES.find(p=>{ 
+        return p.id === placeId
+    });
+    if(!place){
+        return res.status(404).json({message:"couldnot find id"});
+    }
+    res.json({place});
+});
+
+router.get("/user/:uid", (req,res,next)=>{
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find(p=>{
+        return p.creator === userId
+    })
+    res.json({place});
+
+})
 
 module.exports= router;
